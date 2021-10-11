87d56cb3e546bc14527445651355140cc0ce8cab
FatBobSlim <54420301+FatBobSlim@users.noreply.github.com>
2021-10-11 19:03:05 +0100

    Another commit

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -3,7 +3,7 @@
 namespace WishList.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
         public IActionResult Index()
         {
             return View("Index");
